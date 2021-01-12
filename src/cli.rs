261b3e30cf0a4522cb61187fca70ea9b507c4244
Pierre Marijon <pierre.marijon@hhu.de>
2021-01-12 15:22:53 +0100

    Format, clippy and add CI

--- src/cli.rs
+++ src/cli.rs
@@ -5,54 +5,41 @@ use crate::error::Error;
 #[clap(
     version = "0.1",
     author = "Pierre Marijon <pierre.marijon@hhu.de>",
-    about = "Store a kmer list in kff format by use minimizer"
+    about = "Convert a raw kmer count in kff format with a minimizer compression"
 )]
 pub struct Command {
     #[clap(
-	short = 'i',
-	long = "input",
-	about = "Path of the kmer list"
+        short = 'i',
+        long = "input",
+        about = "Path of kmers counts in csv format"
     )]
     pub input: String,
 
-    #[clap(
-	short = 'o',
-	long = "output",
-	about = "Path of the kff file"
-    )]
+    #[clap(short = 'o', long = "output", about = "Path of the kff file")]
     pub output: String,
 
-    #[clap(
-	short = 'k',
-	long = "kmer-size",
-	about = "Kmer size"
-    )]
+    #[clap(short = 'k', long = "kmer-size", about = "Kmer size")]
     pub k: u8,
 
-    #[clap(
-	short = 'm',
-	long = "minimizer-size",
-	about = "Minimizer size"
-    )]
+    #[clap(short = 'm', long = "minimizer-size", about = "Minimizer size")]
     pub m: u8,
 
     #[clap(
-	short = 'p',
-	long = "prefix",
-	about = "Prefix add before temporary file"
+        short = 'p',
+        long = "prefix",
+        about = "Prefix add before temporary file"
     )]
     pub prefix: String,
 }
 
-
 pub fn check_params(params: Command) -> Result<Command, Error> {
     if params.k > 64 {
-	Err(Error::CliKUpperThan64)
+        Err(Error::CliKUpperThan64)
     } else if params.m > 32 {
-	Err(Error::CliMUpperThan32)
+        Err(Error::CliMUpperThan32)
     } else if params.m >= params.k {
-	Err(Error::CliMUpperOrEqualThanK)
+        Err(Error::CliMUpperOrEqualThanK)
     } else {
-	Ok(params)
+        Ok(params)
     }
 }
