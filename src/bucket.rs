1789c7f8f290425956bc1ff13a88a3d08f6df5f9
Pierre Marijon <pierre.marijon@hhu.de>
2021-01-13 11:17:11 +0100

    Move bucket function in specific directory

---
+++ src/bucket.rs
@@ -0,0 +1,82 @@
+/* std use */
+use std::io::Write;
+use std::str::FromStr;
+
+/* crate use */
+use anyhow::{Context, Result};
+
+/* local use */
+use crate::seq2bits;
+
+pub fn create(
+    input: &str,
+    prefix: &str,
+    k: u8,
+    m: u8,
+) -> Result<std::collections::HashSet<u128>> {
+    let mut bob = std::collections::HashSet::<u128>::new();
+
+    let input =
+        std::io::BufReader::new(std::fs::File::open(input).with_context(|| "Open input file")?);
+    let mut reader = csv::ReaderBuilder::new()
+        .delimiter(b',')
+        .has_headers(false)
+        .from_reader(input);
+    let mut iter = reader.records();
+
+    while let Some(Ok(record)) = iter.next() {
+        let mut kmer = seq2bits::seq2bit(record[0].as_bytes());
+        let count = u8::from_str(&record[1])?;
+
+        let (minimizer, _, forward) = seq2bits::get_minimizer(kmer, k, m);
+        kmer = if forward {
+            kmer
+        } else {
+            seq2bits::revcomp(kmer, k)
+        };
+
+        if seq2bits::multiple_mini(kmer, minimizer, k, m) {
+            write_kmer(kmer, count, &format!("{}multiple", prefix))?;
+        } else {
+            write_kmer(kmer, count, &format!("{}{}", prefix, minimizer))?;
+        }
+
+        bob.insert(minimizer);
+    }
+
+    Ok(bob)
+}
+
+
+pub fn write_kmer(kmer: u128, count: u8, filename: &str) -> Result<()> {
+    let mut bucket = if std::path::Path::new(filename).exists() {
+        std::fs::OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(filename)
+            .with_context(|| "Open bucket file")?
+    } else {
+        std::fs::File::create(filename).with_context(|| "Open bucket file")?
+    };
+
+    writeln!(bucket, "{},{}", kmer, count).with_context(|| "Write kmer")?;
+
+    Ok(())
+}
+
+pub fn read(path: String) -> Result<std::collections::HashMap<u128, u8>> {
+    let mut res = std::collections::HashMap::new();
+
+    let input = std::io::BufReader::new(std::fs::File::open(path)?);
+    let mut reader = csv::ReaderBuilder::new()
+        .delimiter(b',')
+        .has_headers(false)
+        .from_reader(input);
+    let mut iter = reader.records();
+
+    while let Some(Ok(record)) = iter.next() {
+        res.insert(u128::from_str(&record[0])?, u8::from_str(&record[1])?);
+    }
+
+    Ok(res)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-/* std use */
-use std::io::Write;
-use std::str::FromStr;
-
 /* crate use */
 use anyhow::{anyhow, Context, Result};
 use clap::Clap;
@@ -9,13 +5,14 @@ use clap::Clap;
 /* mod declaration */
 mod cli;
 mod error;
+mod bucket;
 mod seq2bits;
 
 fn main() -> Result<()> {
     let params = cli::check_params(cli::Command::parse()).with_context(|| "Check parameter")?;
 
     // generate bucket
-    let bob = create_bucket(&params.input, &params.prefix, params.k, params.m)?;
+    let bob = bucket::create(&params.input, &params.prefix, params.k, params.m)?;
 
     // create kff
     let mut writer = kff::Writer::new(std::fs::File::create(params.output)?, 0b00011011, b"")?;
@@ -29,7 +26,7 @@ fn main() -> Result<()> {
 
     // iterate over bucket
     for b_id in bob.iter() {
-        let bucket = read_bucket(format!("{}{}", params.prefix, b_id))?;
+        let bucket = bucket::read(format!("{}{}", params.prefix, b_id))?;
         let mut seens = std::collections::HashSet::new();
 
         let mut mini_poss = Vec::new();
@@ -104,7 +101,7 @@ fn main() -> Result<()> {
         )?;
     }
 
-    let bucket = read_bucket(format!("{}multiple", params.prefix))?;
+    let bucket = bucket::read(format!("{}multiple", params.prefix))?;
     let mut sequences = Vec::new();
     let mut datas = Vec::new();
 
@@ -174,77 +171,4 @@ fn successor(
     }
 
     None
-}
-
-fn create_bucket(
-    input: &str,
-    prefix: &str,
-    k: u8,
-    m: u8,
-) -> Result<std::collections::HashSet<u128>> {
-    let mut bob = std::collections::HashSet::<u128>::new();
-
-    let input =
-        std::io::BufReader::new(std::fs::File::open(input).with_context(|| "Open input file")?);
-    let mut reader = csv::ReaderBuilder::new()
-        .delimiter(b',')
-        .has_headers(false)
-        .from_reader(input);
-    let mut iter = reader.records();
-
-    while let Some(Ok(record)) = iter.next() {
-        let mut kmer = seq2bits::seq2bit(record[0].as_bytes());
-        let count = u8::from_str(&record[1])?;
-
-        let (minimizer, _, forward) = seq2bits::get_minimizer(kmer, k, m);
-        kmer = if forward {
-            kmer
-        } else {
-            seq2bits::revcomp(kmer, k)
-        };
-
-        if seq2bits::multiple_mini(kmer, minimizer, k, m) {
-            write_kmer(kmer, count, &format!("{}multiple", prefix))?;
-        } else {
-            write_kmer(kmer, count, &format!("{}{}", prefix, minimizer))?;
-        }
-
-        bob.insert(minimizer);
-    }
-
-    Ok(bob)
-}
-
-
-fn write_kmer(kmer: u128, count: u8, filename: &str) -> Result<()> {
-    let mut bucket = if std::path::Path::new(filename).exists() {
-        std::fs::OpenOptions::new()
-            .append(true)
-            .create(true)
-            .open(filename)
-            .with_context(|| "Open bucket file")?
-    } else {
-        std::fs::File::create(filename).with_context(|| "Open bucket file")?
-    };
-
-    writeln!(bucket, "{},{}", kmer, count).with_context(|| "Write kmer")?;
-
-    Ok(())
-}
-
-fn read_bucket(path: String) -> Result<std::collections::HashMap<u128, u8>> {
-    let mut res = std::collections::HashMap::new();
-
-    let input = std::io::BufReader::new(std::fs::File::open(path)?);
-    let mut reader = csv::ReaderBuilder::new()
-        .delimiter(b',')
-        .has_headers(false)
-        .from_reader(input);
-    let mut iter = reader.records();
-
-    while let Some(Ok(record)) = iter.next() {
-        res.insert(u128::from_str(&record[0])?, u8::from_str(&record[1])?);
-    }
-
-    Ok(res)
 }
